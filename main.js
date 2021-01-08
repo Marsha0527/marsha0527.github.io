20cf8c498cd5f419a51a8a9893f79e292a8a0738
Marsha Woods <68514405+Marsha0527@users.noreply.github.com>
2021-01-08 07:44:49 -0600

    added smooth scroll

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 //Smooth Scrolling function
 
-const links = document.querySelectorAll(".#main-nav li a);
- 
-  for (const link of links) {
-  link.addEventListener("click", smoothScroll);
-}
-
-function smoothScroll(e) {
-    e.preventDefault();
-  const href = this.getAttribute("href");
-  
-  document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-
+const scroll = new SmoothScroll('.main-nav a[href*="#"]',{
+ speed: 800
+});
