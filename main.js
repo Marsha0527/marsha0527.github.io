0e34bd702fc16dd366dca7df546d262df56d5ee3
Marsha Woods <68514405+Marsha0527@users.noreply.github.com>
2021-01-08 06:43:11 -0600

    added smooth scoll

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+//Smooth Scrolling function
+
+const links = document.querySelectorAll(".#main-nav li a);
+ 
+  for (const link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+  const href = this.getAttribute("href");
+  
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
 
