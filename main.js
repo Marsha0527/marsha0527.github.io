78ff04d4954e36f973b40568b7a474268b434e54
Marsha Woods <68514405+Marsha0527@users.noreply.github.com>
2021-01-16 15:07:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Moving BackgroundW mouse position
 
 const el = document.querySelector("#module");
+
 el.addEventListener("mousemove", (e) => {
  el.style.setProperty('--x', -e.offsetX + "px");
- el.style.setProperty('--y',-e.offsetY + "px");
+ el.style.setProperty('--y', -e.offsetY + "px");
 });
 
 //Smooth Scrolling function
