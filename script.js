1022770de496ebac0349de78e67a79e785609e18
Marsha Woods <68514405+Marsha0527@users.noreply.github.com>
2021-03-22 11:26:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Get modal element
+const modal = document.getElementById('simpleModal');
+// Get open modal button
+const modalBtn = document.getElementById('modalBtn');
+// Get close button
+const closeBtn = document.getElementById('closeBtn');
 
+// Listen for click
+modalBtn.addEventListener('click', openModal);
+
+// Function to open Modal
+function openModal(){
+  console.log(123);
+}
