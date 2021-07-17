d0667b34d1e18b0d8d35ba2694ae984391440961
Marsha Woods <68514405+Marsha0527@users.noreply.github.com>
2021-07-16 21:13:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-// Get modal element
-const modal = document.getElementById('simpleModal');
-// Get open modal button
-const modalBtn = document.getElementById('modalBtn');
-// Get close button
-const closeBtn = document.getElementsByClassName('closeBtn');
+//Modal Element
+const inputs = document.querySelectorAll (".input");
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click('click',closeModal)[0];
-closeBtn.addEventListener
-// Listen for outside click
-window.addEventListener('click', outsideClick);
+function focusFunc() {
+ let parent = this.parentNode;
+ parent.classList.add("focus");
 
-
-// Function to open Modal
-function openModal() {
- modal.style.display = 'block';
-}
-
-// Function to close modal
-function closeModal() {
- modal.style.display = 'none';
-}
-
-// Function to close modal if outside click
-function outsideClick(e) {
- if(e.target == modal) {
-  modal.style.display = 'none';
- }
-}
+inputs.forEach(input => {
+input.addEventListener("focus", focusFunc);
+});
